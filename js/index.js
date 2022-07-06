11f547e2c2b222cf0055a955291b939f26110c5b
irakliqiria <irakliqiria8@gmail.com>
2022-07-06 03:15:13 -0700

    add back button function

--- js/index.js
+++ js/index.js
@@ -1,24 +1,42 @@
 import leftSideContent from "./components/leftSide.js"
 import rightSideContent from "./components/rightSide.js"
 // import changeStep from "./functions.js"
-
+let stepCount = 0;
 const leftSideContainer = document.getElementById("left_side");
-// const rightSideContainer = document.getElementById("right_side")
 const getStartedButton = document.getElementById("get_started")
 const prevStep = document.getElementById("prev_step")
 const nextStep = document.getElementById("next_step")
+const homePageRow = document.querySelector(".home_page_row")
+const registerRow = document.querySelector(".register_row ")
 
-prevStep.addEventListener("click",() => {
+getStartedButton.addEventListener("click", (e) => {
+    stepCount++
+    homePageRow.classList.add("disabled")
+    registerRow.classList.remove("disabled")
 
 })
 
-getStartedButton.addEventListener("click", (e) => {
-    // const rightSideContainer = document.querySelectorAll(".right_side_card")
-    // const setArguments = {text: "When you see a good move, look for a better one.",
-    //                         author: "Emanuel Lasker", image: "second.png", position: "left"}
-    // leftSideContainer.innerHTML = leftSideContent(setArguments)
-    // // rightSideContainer.innerHTML = rightSideContent()
+prevStep.addEventListener("click", (e) => {
     
+    if (stepCount === 1) {
+        stepCount--
+        homePageRow.classList.remove("disabled")
+        registerRow.classList.add("disabled")
+    } else {
+        
+        switch(stepCount) {
+            case 2:
+                const content = {text: "When you see a good move, look for a better one.",
+                                author: "Emanuel Lasker", image: "sec.png", position: "left"}
+                leftSideContainer.innerHTML = leftSideContent(content)
+              break;
+            case 3:
+              // code block
+              break;
+            default:
+              // code block
+          } 
+    }
 })
 
 // form input fields
